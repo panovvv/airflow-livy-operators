30e0898f29503a098e67a7482f00c10230604f7c
Vadim Panov <headcra6@gmail.com>
2021-05-07 13:30:30 +0200

    0.4.2 - include supported versions via trove classifiers, update badges

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as fh:
 
 setup(
     name="airflow-livy-operators",
-    version="0.4.1",
+    version="0.4.2",
     author="Vadim Panov",
     author_email="headcra6@gmail.com",
     description="Lets Airflow DAGs run Spark jobs via Livy: sessions and/or batches.",
@@ -16,4 +16,9 @@ setup(
     packages=["airflow_livy"],
     package_dir={"airflow_livy": "airflow_home/plugins/airflow_livy"},
     python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
 )
